2e0fc43a611945c51e564ece1f57e7aeef7a422a
prasannjeet <prasannjeetsingh@gmail.com>
2018-03-22 00:04:14 +0100

    fixed scrolling and mobile menu

--- portfolio/scripts.js
+++ portfolio/scripts.js
@@ -3,20 +3,24 @@ function scrollUp() {
 }
 
 $(document).ready(function () {
-   $("#social-links").find("a").addClass("mx-3");
-   $("ul#navButtons").find("li").find("a").addClass("p-4").addClass("mx-1");
-   $(".scrollButton").click(function () {
+    $("#social-links").find("a").addClass("mx-3");
+    $("ul#navButtons").find("li").find("a").addClass("p-4").addClass("mx-1");
+    $(".scrollButton").click(function () {
+        var x = -1; //y axis displacement
+        var y = 1; //delay in milliseconds
 
-          var x = -1; //y axis displacement
-          var y = 1; //delay in milliseconds
+        var someID = setInterval(function () {
+            window.scrollBy(0, x);
+            //x = x - 1;
+        }, y);
 
-          var someID = setInterval(function() {
-              window.scrollBy(0, x);
-              //x = x - 1;
-          }, y);
+        setTimeout(function () {
+            clearInterval(someID);
+        }, 350)
 
-          setTimeout(function(){
-              clearInterval(someID);
-          }, 350)
-   });
+        $(".navbar-collapse").collapse("toggle");
+    });
+    $("#socialButton").click(function () {
+        $(".navbar-collapse").collapse("toggle");
+    })
 });
