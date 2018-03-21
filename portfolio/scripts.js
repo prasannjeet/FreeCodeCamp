00ffd08a33af0f7b0f2f790f50c13412280ba66f
prasannjeet <prasannjeetsingh@gmail.com>
2018-03-21 23:42:37 +0100

    portfolio page prototype

---
+++ portfolio/scripts.js
@@ -0,0 +1,22 @@
+function scrollUp() {
+
+}
+
+$(document).ready(function () {
+   $("#social-links").find("a").addClass("mx-3");
+   $("ul#navButtons").find("li").find("a").addClass("p-4").addClass("mx-1");
+   $(".scrollButton").click(function () {
+
+          var x = -1; //y axis displacement
+          var y = 1; //delay in milliseconds
+
+          var someID = setInterval(function() {
+              window.scrollBy(0, x);
+              //x = x - 1;
+          }, y);
+
+          setTimeout(function(){
+              clearInterval(someID);
+          }, 350)
+   });
+});
